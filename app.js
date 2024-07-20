d425f1b380b85e99863bc466496d082f8534625b
Andrew V D <andrewvd31@gmail.com>
2024-07-20 11:11:38 +0530

    Update v1.0

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-const clearBtn = document.querySelector('.clear-btn')
-const backBtn = document.querySelector('.backsp-btn')
-const equalToBtn = document.querySelector('.equal-to')
-const btnClick = document.querySelectorAll('.btn')
-const inputbtnValue = document.querySelector('.number-box')
+const clearBtn = document.querySelector(".clear-btn");
+const backBtn = document.querySelector(".backsp-btn");
+const equalToBtn = document.querySelector(".equal-to");
+const btnClick = document.querySelectorAll(".btn");
+const inputbtnValue = document.querySelector(".number-box");
 
-btnClick.forEach(myFunction) 
+btnClick.forEach(myFunction);
 
-equalToBtn.addEventListener('click',evaluateFunction)
+equalToBtn.addEventListener("click", evaluateFunction);
 
-backBtn.addEventListener('click',backSpaceFunction)
+backBtn.addEventListener("click", backSpaceFunction);
 
-clearBtn.addEventListener('click',clearFunction)
+clearBtn.addEventListener("click", clearFunction);
 
-function buttonFunctionDisable(data){
-    for (let buttons of btnClick){
-        buttons.disabled = data
+function buttonFunctionDisable(data) {
+  for (let buttons of btnClick) {
+    buttons.disabled = data;
+  }
+}
+
+function evaluateFunction() {
+  if (inputbtnValue.value) {
+    const equalTo = eval(inputbtnValue.value);
+    inputbtnValue.value = equalTo;
+    buttonFunctionDisable(false);
+  }
+}
+
+function clearFunction() {
+  inputbtnValue.value = "";
+}
+
+function backSpaceFunction() {
+  inputbtnValue.value = inputbtnValue.value.slice(0, -1);
+  buttonFunctionDisable(false);
+}
+
+function myFunction(button) {
+  button.addEventListener("click", (e) => {
+    const btnClick = e.target.dataset.set;
+    if (btnClick) {
+      inputbtnValue.value += btnClick;
     }
+    if (inputbtnValue.value.length === inputbtnValue.maxLength) {
+      buttonFunctionDisable(true);
+    }
+  });
 }
-
-function evaluateFunction(){
-    const equalTo = eval(inputbtnValue.value)
-    inputbtnValue.value = equalTo 
-    buttonFunctionDisable(false)
-}
-
-function clearFunction(){
-    inputbtnValue.value = ""
-}
-
-function backSpaceFunction(){
-    inputbtnValue.value = inputbtnValue.value.slice(0,-1)
-    buttonFunctionDisable(false)
-}
-
-function myFunction(button){
-    button.addEventListener('click',(e)=>{
-        const btnClick = e.target.dataset.set
-        if (btnClick){
-            inputbtnValue.value += btnClick
-        }
-        if(inputbtnValue.value.length === inputbtnValue.maxLength){
-            buttonFunctionDisable(true)
-        }
-    })
-}
-
